71743bec01b798b78dee52f94e4495ff90c95ae2
Rob Anderson <Mctalian@users.noreply.github.com>
2025-03-16 10:50:23 -0600

    feat: spec-specific layout presets (#10)

--- Core.lua
+++ Core.lua
@@ -9,6 +9,52 @@ local DLP = LibStub("AceAddon-3.0"):NewAddon(addonName, "AceConsole-3.0", "AceEv
 
 G_DLP.DLP = DLP
 
+-- From https://github.com/Gethe/wow-ui-source/blob/live/Interface/AddOns/Blizzard_PlayerSpells/ClassSpecializations/Blizzard_ClassSpecializationsFrame.lua#L13C1-L55C2
+-- Will need to keep an eye out for changes in future patches
+local SPEC_TEXTURE_FORMAT = "spec-thumbnail-%s";
+
+local SPEC_FORMAT_STRINGS = {
+	[62] = "mage-arcane",
+	[63] = "mage-fire",
+	[64] = "mage-frost",
+	[65] = "paladin-holy",
+	[66] = "paladin-protection",
+	[70] = "paladin-retribution",
+	[71] = "warrior-arms",
+	[72] = "warrior-fury",
+	[73] = "warrior-protection",
+	[102] = "druid-balance",
+	[103] = "druid-feral",
+	[104] = "druid-guardian",
+	[105] = "druid-restoration",
+	[250] = "deathknight-blood",
+	[251] = "deathknight-frost",
+	[252] = "deathknight-unholy",
+	[253] = "hunter-beastmastery",
+	[254] = "hunter-marksmanship",
+	[255] = "hunter-survival",
+	[256] = "priest-discipline",
+	[257] = "priest-holy",
+	[258] = "priest-shadow",
+	[259] = "rogue-assassination",
+	[260] = "rogue-outlaw",
+	[261] = "rogue-subtlety",
+	[262] = "shaman-elemental",
+	[263] = "shaman-enhancement",
+	[264] = "shaman-restoration",
+	[265] = "warlock-affliction",
+	[266] = "warlock-demonology",
+	[267] = "warlock-destruction",
+	[268] = "monk-brewmaster",
+	[269] = "monk-windwalker",
+	[270] = "monk-mistweaver",
+	[577] = "demonhunter-havoc",
+	[581] = "demonhunter-vengeance",
+	[1467] = "evoker-devastation",
+	[1468] = "evoker-preservation",
+	[1473] = "evoker-augmentation",
+}
+
 ---@type AceConfigDialog-3.0
 local acd = LibStub("AceConfigDialog-3.0")
 
@@ -59,25 +105,49 @@ local options = {
             end,
             get = "GetPreset",
             set = "SetPreset",
-            order = -1
+            order = 2,
+        },
+        specs = {
+            type = "group",
+            name = G_DLP.L["OPTIONS_SPECS_NAME"],
+            inline = true,
+            args = {
+                desc = {
+                    type = "description",
+                    name = G_DLP.L["OPTIONS_SPECS_DESC"],
+                    order = 1
+                },
+            },
+            order = 3
         }
     }
 }
 
+local layouts = nil
+local repoUrl = "https://github.com/McTalian/DeviceLayoutPreset"
+local currentVersion = "@project-version@"
+local SPEC_DEFAULT = -1
+local DEVICE_DEFAULT = 0
+
 local defaults = {
     global = {
-        presetIndexOnLogin = 0,
+        presetIndexOnLogin = DEVICE_DEFAULT,
         lastVersionLoaded = "v1.0.0"
-    }
+    },
+    char = {
+        specs = {
+            overrides = {}
+        }
+    },
 }
-
-local layouts = nil
 
 function DLP:OnInitialize()
     self.db = LibStub("AceDB-3.0"):New("DeviceLayoutPresetDB", defaults, true)
     LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, options)
     self:InitializeOptions()
+    self:RegisterEvent("PLAYER_ENTERING_WORLD")
     self.bucketHandle = self:RegisterBucketEvent("EDIT_MODE_LAYOUTS_UPDATED", 0.2, "EDIT_MODE_LAYOUTS_UPDATED")
+    self:RegisterBucketEvent("PLAYER_SPECIALIZATION_CHANGED", 0.2)
     self:SecureHook(C_EditMode, "OnLayoutDeleted", "OnLayoutDeleted")
     self:SecureHook(C_EditMode, "OnLayoutAdded", "OnLayoutAdded")
     self:RegisterChatCommand("dlp", "SlashCommand")
@@ -85,8 +155,6 @@ function DLP:OnInitialize()
     self:RegisterChatCommand("deviceLayoutPreset", "SlashCommand")
 end
 
-local repoUrl = "https://github.com/McTalian/DeviceLayoutPreset"
-local currentVersion = "@project-version@"
 function DLP:EDIT_MODE_LAYOUTS_UPDATED(bucketedArgs)
     local layoutInfo = nil
     for k, v in pairs(bucketedArgs) do
@@ -102,13 +170,27 @@ function DLP:EDIT_MODE_LAYOUTS_UPDATED(bucketedArgs)
     end
 
     local desired = self.db.global.presetIndexOnLogin
-    layouts = EditModeManagerFrame:GetLayouts()
-    if desired <= 0 or desired > #layouts then
+    local type = G_DLP.L["LAYOUT_TYPE_DEVICE"]
+    local specId = PlayerUtil.GetCurrentSpecID()
+    if self.db.char.specs.overrides[specId] ~= nil then
+        desired = self.db.char.specs[specId]
+        type = G_DLP.L["LAYOUT_TYPE_SPEC"]
+    else
         self:Print(G_DLP.L["ERROR_LAYOUT_INVALID"])
-        self.db.global.presetIndexOnLogin = 0
+        self.db.char.specs.overrides[specId] = SPEC_DEFAULT
+        return
+    end
+    if desired == SPEC_DEFAULT then
+        desired = self.db.global.presetIndexOnLogin
+        type = G_DLP.L["LAYOUT_TYPE_DEVICE"]
+    end
+    layouts = EditModeManagerFrame:GetLayouts()
+    if desired <= DEVICE_DEFAULT or desired > #layouts then
+        self:Print(G_DLP.L["ERROR_LAYOUT_INVALID"])
+        self.db.global.presetIndexOnLogin = DEVICE_DEFAULT
     elseif layoutInfo.activeLayout ~= desired then
         EditModeManagerFrame:SelectLayout(desired)
-        self:Printf(G_DLP.L["SUCCESS_LOADED_LAYOUT"], layouts[desired].layoutName)
+        self:Printf(G_DLP.L["SUCCESS_LOADED_LAYOUT"], type, layouts[desired].layoutName)
     else
         local isNewVersion = currentVersion ~= self.db.global.lastVersionLoaded
         if isNewVersion then
@@ -119,20 +201,121 @@ function DLP:EDIT_MODE_LAYOUTS_UPDATED(bucketedArgs)
     self:UnregisterBucket(self.bucketHandle)
 end
 
+---On talent spec changes, load the spec override layout or the default device layout if no override is set
+---@param eventName string
+---@param unitTarget UnitToken 
+function DLP:PLAYER_SPECIALIZATION_CHANGED(eventName, unitTarget)
+    if unitTarget ~= "player" then
+        return
+    end
+    local type = G_DLP.L["LAYOUT_TYPE_SPEC"]
+    local specId = PlayerUtil.GetCurrentSpecID()
+    if self.db.char.specs.overrides[specId] == nil then
+        self.db.char.specs.overrides[specId] = SPEC_DEFAULT
+        return
+    end
+    layouts = EditModeManagerFrame:GetLayouts()
+    local desired = self.db.char.specs.overrides[specId]
+    if desired == SPEC_DEFAULT then
+        desired = self.db.global.presetIndexOnLogin
+        type = G_DLP.L["LAYOUT_TYPE_DEVICE"]
+    end
+    if desired <= DEVICE_DEFAULT or desired > #layouts then
+        self:Print(G_DLP.L["ERROR_LAYOUT_INVALID"])
+        self.db.char.specs.overrides[specId] = SPEC_DEFAULT
+    else
+        EditModeManagerFrame:SelectLayout(desired)
+        self:Printf(G_DLP.L["SUCCESS_LOADED_LAYOUT"], type, layouts[desired].layoutName)
+    end
+end
+
+---@class Spec
+---@field specId number
+---@field specName string
+---@field specDesc string
+---@field specIcon number
+---@field specRole string
+---@field specPrimaryStat number
+---@field specAtlasString string
+
+---On initial login, get the specs and create the spec override options
+---@param event string
+---@param isLogin boolean
+---@param isReload boolean
+function DLP:PLAYER_ENTERING_WORLD(event, isLogin, isReload)
+    local numSpecs = GetNumSpecializations()
+    ---@type table<number, Spec>
+    self.specs = {}
+    for i = 1, numSpecs do
+        local specID, specName, specDesc, specIcon, specRole, specPrimaryStat = GetSpecializationInfo(i)
+        self.specs[specID] = {
+            specId = specID,
+            specName = specName,
+            specDesc = specDesc,
+            specIcon = specIcon,
+            specRole = specRole,
+            specPrimaryStat = specPrimaryStat,
+            specAtlasString = string.format(
+                "|A:%s:16:16:0:0|a",
+                string.format(SPEC_TEXTURE_FORMAT, SPEC_FORMAT_STRINGS[specID])
+            )
+        }
+    end
+
+    local i = 0
+    for k, v in pairs(self.specs) do
+        i = i + 1
+        if self.db.char.specs.overrides[k] == nil then
+            self.db.char.specs.overrides[k] = SPEC_DEFAULT
+        end
+        options.args.specs.args["spec" .. k] = {
+            type = "select",
+            name = v.specName .. " " .. v.specAtlasString,
+            desc = string.format(G_DLP.L["OPTIONS_SPECS_PRESET_DESC"], v.specName),
+            values = function()
+                return DLP:GetLayouts(true)
+            end,
+            get = function()
+                return DLP.db.char.specs.overrides[k]
+            end,
+            set = function(info, value)
+                DLP.db.char.specs.overrides[k] = value
+            end,
+            width = 1.5,
+            order = 1 + i
+        }
+        i = i + 1
+        options.args.specs.args["spacer" .. k] = {
+            type = "description",
+            name = "",
+            order = 1 + i
+        }
+    end
+
+    LibStub("AceConfigRegistry-3.0"):NotifyChange(addonName)
+end
+
 function DLP:OnLayoutDeleted(deletedIndex)
     if deletedIndex == self.db.global.presetIndexOnLogin then
         self:Print(G_DLP.L["EVENT_DELETED_LAYOUT"])
-        self.db.global.presetIndexOnLogin = 0
+        self.db.global.presetIndexOnLogin = DEVICE_DEFAULT
     end
+    LibStub("AceConfigRegistry-3.0"):NotifyChange(addonName)
 end
 
 function DLP:OnLayoutAdded()
     self:Print(G_DLP.L["EVENT_CREATED_LAYOUT"])
+    LibStub("AceConfigRegistry-3.0"):NotifyChange(addonName)
 end
 
-function DLP:GetLayouts()
+---Get EditMode layouts
+---@param forSpecs? boolean for specs, we will add a "Same as above" option
+function DLP:GetLayouts(forSpecs)
     layouts = EditModeManagerFrame:GetLayouts()
     local values = {}
+    if forSpecs then
+        values[SPEC_DEFAULT] = G_DLP.L["DO_NOT_OVERRIDE"]
+    end
     for i, l in ipairs(layouts) do
         values[i] = l.layoutName
     end
@@ -141,7 +324,7 @@ end
 
 function DLP:InitializeOptions()
     if self.optionsFrame == nil then
-        self.optionsFrame = acd:AddToBlizOptions(addonName, addonName)
+        self.optionsFrame = acd:AddToBlizOptions(addonName)
     end
 end
 
--- locale/main.lua
+++ locale/main.lua
@@ -4,4 +4,5 @@ local _, ns = ...
 ---@class DLP_ns
 local G_DLP = ns
 
+---@class DLP_Locale
 G_DLP.L = LibStub("AceLocale-3.0"):GetLocale(G_DLP.localeName)
